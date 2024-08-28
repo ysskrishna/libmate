ff65eda60069361025a4c3c8caec9cb98a4adc2f
ysskrishna <sivasaikrishnassk@gmail.com>
2024-08-29 02:15:18 +0530

    Siva | add genre constant

--- frontend/src/common/constants.js
+++ frontend/src/common/constants.js
@@ -2,3 +2,15 @@ export const ROLE = {
     ADMIN: 'admin',
     USER: 'user'
 }
+
+export const BookGenre = {
+    FICTION: "Fiction",
+    NON_FICTION: "Non-Fiction",
+    MYSTERY: "Mystery",
+    FANTASY: "Fantasy",
+    SCIENCE_FICTION: "Science Fiction",
+    BIOGRAPHY: "Biography",
+    HISTORY: "History",
+    ROMANCE: "Romance",
+    THRILLER: "Thriller"
+}
